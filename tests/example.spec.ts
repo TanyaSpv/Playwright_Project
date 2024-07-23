7835e7bfe7c10bdb4029ca37ff73e143c97c1276
Tanya-design93 <67875354+Tanya-design93@users.noreply.github.com>
2024-07-23 20:11:45 -0300

    T-shirts were sorted by price from low to high

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,31 +1,68 @@
-import { test, expect } from '@playwright/test';
-import { faker } from '@faker-js/faker';
-import dotenv from 'dotenv';
-const stealth = require('puppeteer-extra-plugin-stealth')();
-const { chromium } = require('playwright-extra');
-
-// Load the stealth plugin and use defaults
-chromium.use(stealth);
+import { test, expect } from "@playwright/test";
+import dotenv from "dotenv";
 
 // Read from default ".env" file.
 dotenv.config();
 
-
 test.beforeEach(async ({ page }) => {
-  await page.goto('https://www.uniqlo.com/ca/en/');
+  await page.goto("https://www.uniqlo.com/ca/en/");
   await page.waitForTimeout(3000);
   await page.keyboard.press("Escape");
-  await page.getByRole('link', { name: 'Login' }).click();
-  await page.getByPlaceholder('Enter a valid email').fill(`${process.env.EMAIL}`);
-  await page.getByLabel('Password Password must be at').fill(`${process.env.PASSWORD}`);
+  await page.getByRole("link", { name: "Login" }).click();
+  await page
+    .getByPlaceholder("Enter a valid email")
+    .fill(`${process.env.EMAIL}`);
+  await page
+    .getByLabel("Password Password must be at")
+    .fill(`${process.env.PASSWORD}`);
   await page.locator('[data-test="login-button"]').click();
 });
 
-test('searching for a keyboard', async ({ page }) => {
-  await page.getByRole('button', { name: 'Search' }).click();
-  await page.getByPlaceholder('Search by keyword').fill('hats');
+test("searching for a hat", async ({ page }) => {
+  await page.getByRole("button", { name: "Search" }).click();
+  await page.getByPlaceholder("Search by keyword").fill("hats");
 
   await page.keyboard.press("Enter");
   await page.waitForLoadState();
-  await expect(page.getByRole('link', { name: 'CASHMERE KNITTED BEANIE' })).toBeVisible();
+  await expect(
+    page.getByRole("link", { name: "CASHMERE KNITTED BEANIE" })
+  ).toBeVisible();
+});
+
+test("Selecting womens T-shirts and filtering and sorting them", async ({
+  page,
+}) => {
+  await page.getByRole("link", { name: "women" }).hover();
+  await page
+    .locator("a")
+    .filter({ hasText: /^T-shirts$/ })
+    .first()
+    .click();
+  await page.locator('[data-test="sort-by"]').click();
+  await page.waitForTimeout(1000);
+  await page.getByRole("option", { name: "Price: Low to high" }).click();
+  await page.waitForTimeout(1000);
+  var firstItemElement = await page
+    .locator(
+      `//article[contains(@class, 'fr-grid-item w4')]//div[@class='price fr-no-uppercase']`
+    )
+    .first()
+    .allInnerTexts();
+  var lastItemElement = await page
+    .locator(
+      `//article[contains(@class, 'fr-grid-item w4')]//div[@class='price fr-no-uppercase']`
+    )
+    .last()
+    .allInnerTexts();
+
+  // Step 1: Remove non-digit and non-dot characters
+  var cleanedString = firstItemElement[0].replace(/[^\d.]/g, "");
+
+  // Step 2: Parse the cleaned string as a floating-point number
+  var firstItemValue = parseFloat(cleanedString);
+
+  cleanedString = lastItemElement[0].replace(/[^\d.]/g, "");
+  var lastItemValue = parseFloat(cleanedString);
+
+  expect(firstItemValue).toBeLessThanOrEqual(lastItemValue);
 });
