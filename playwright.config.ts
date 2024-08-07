7b4c58d026c3cd94fe177f7473c77d2cef98b08a
Tanya-design93 <67875354+Tanya-design93@users.noreply.github.com>
2024-08-07 20:15:21 -0300

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 0 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? "1" : "90%",
+  workers: process.env.CI ? 1 : "90%",
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? [['html'], ['dot']] : [['allure-playwright']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
